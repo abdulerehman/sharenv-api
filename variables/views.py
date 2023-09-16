4e0ebe7d33595f3f0394365c69fcbcbf6dcee501
abdulrehmandev388 <abdulrehmandev388@gmail.com>
2023-09-16 19:29:54 -0400

    add: the  logic of get function in VariablesView

--- variables/views.py
+++ variables/views.py
@@ -1,15 +1,29 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-from .models import Permissions, User, Variables
+from .models import User, Variables
 from .provider import sign_with_google
 
 class VariablesView(APIView):
-    permission_classes = [IsAuthenticated]
 
 
     def get(self, request, id=None):
-        return Response({})
+        if id:
+            variables = Variables.objects.filter(id=id)
+            if variables.exists():
+                if variables.password:
+                    # get password
+                    return Response({})
+                return Response(data={"type":"success","variables": variables.variables})
+            return Response(data={"type":"error", "message":"variables not found"})                
+        if request.user.is_authenticated:
+            variables = Variables.objects.filter(user_id=request.user.id)
+            data = {
+                "type":"success",
+                "data":variables.values('variables', "password")    
+            }
+            return Response(data=data)
+        return Response(data={"type":"error", "message":"please login"})
 
     def put(self, request, id):
         return Response({})
@@ -33,15 +47,3 @@ class SignUpWithProviderView(APIView):
                 return Response({"type": "error", "message": "Couldn't find your email try again"})
             return Response({"type": "success", "data": user_token})
         return Response({"type": "error", "message": "Provider not found"})
-
-
-
-
-class PermissionsView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def delete(self, request, id):
-        return Response({})
-
-    def post(self, request):
-        return Response({})
